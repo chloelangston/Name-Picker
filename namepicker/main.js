a1cfc4ff7581095682ea2916576f587f49afcafb
chloelangston <chloealangston@gmail.com>
2015-06-24 18:03:14 -0700

    name picker

---
+++ namepicker/main.js
@@ -0,0 +1,6 @@
+var wdi_17_names = ["Addison", "Ben", "Christine", "Chloe", "Cris", "Dane", "David H", "David Z", "Emily", "Imtaek", "Jack", "Jesse P", "Jesse Z", "Jonathan", "Pericles", "Peter", "Philippe", "Sergio", "Simon", "Tali", "Tavo", "Trevor"];
+
+
+var pick = Math.floor(Math.random() * wdi_17_names.length)
+var namePicker = wdi_17_names[pick];
+console.log(namePicker);
